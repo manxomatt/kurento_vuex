0d07eac153fbf8b9a8a7a755090e7eb00ec0d4ad
manx o matt <manxomatt@gmail.com>
2018-10-24 22:10:42 +0700

    Modify Audiocall.vue

--- src/components/Audiocall.vue
+++ src/components/Audiocall.vue
@@ -1,62 +1,61 @@
+/* eslint-disable */
+
 <template>
   <div id="app">
-		<label class="form-label">User Name</label>
-		<input class="input-text" v-model='userName' type="text">            
-		<button @click.prevent='register'>Register</button>
-		<label>Peer Name</label>
-		<input class="form-input"  v-model='peerName' type="text">
-		<button class="btn btn-primary" @click.prevent='call'>Call</button>
-		<button class="btn btn-primary" @click.prevent='endcall'>End Call</button>
-		
-		<div id="videoBig">
-			<audio id="audioOutput" autoplay controls muted></audio>
-			<!-- <video id="videoOutput" autoplay width="640px" height="480px" poster="img/webrtc.png"></video> -->
-		</div>
-		<div id="videoSmall">
-			<audio id="audioInput" autoplay controls muted></audio>
-			<!-- <video id="videoInput" autoplay width="240px" height="180px" poster="img/webrtc.png"></video> -->
-		</div>
-		<!-- div class="counter"> {{ counter }} </div>
-		<button @click="increment" class="increment"> Tambah </button>
-		<button @click="decrement" class="decrement"> Kurangi </button -->
-		
+    <label class="form-label">User Name</label>
+    <input class="input-text" v-model='userName' type="text">
+    <button @click.prevent='register'>Register</button>
+    <label>Peer Name</label>
+    <input class="form-input"  v-model='peerName' type="text">
+    <button class="btn btn-primary" @click.prevent='call'>Call</button>
+    <button class="btn btn-primary" @click.prevent='endcall'>End Call</button>
+    <div id="videoBig">
+      <audio id="audioOutput" autoplay controls muted></audio>
+      <!-- <video id="videoOutput" autoplay width="640px" height="480px" poster="img/webrtc.png"></video> -->
+    </div>
+    <div id="videoSmall">
+    <audio id="audioInput" autoplay controls muted></audio>
+    <!-- <video id="videoInput" autoplay width="240px" height="180px" poster="img/webrtc.png"></video> -->
+    </div>
+    <!-- div class="counter"> {{ counter }} </div>
+    <button @click="increment" class="increment"> Tambah </button>
+    <button @click="decrement" class="decrement"> Kurangi </button -->
+
   </div>
 </template>
 
 <script>
 import '@/assets/css/styles.css'
 export default {
-	data() {
-        return {
-            userName: '',
-            roomName: '',
-            peerName:''
-        };
+  data () {
+    return {
+      userName: '',
+      roomName: '',
+      peerName: ''
+    }
+  },
+  methods: {
+    increment () {
+      this.$store.commit('increment')
     },
-	methods: {
-		increment() {
-			this.$store.commit('increment')
-		},
-		decrement() {
-			this.$store.commit('decrement')
-		},
-		register() {
-            this.$store.dispatch('register', { user: this.userName});
-           
-        },
-        call(){
-            this.$store.dispatch('call', { user: this.userName, peer: this.peerName });
-        },
-		endcall(){
-			this.$store.dispatch('endcall',{});
-		}
-		
-	}, 
-	computed: {
-		counter() {
-			return this.$store.getters.getCounter
-		}
-	}
+    decrement () {
+      this.$store.commit('decrement')
+    },
+    register () {
+      this.$store.dispatch('register', {user: this.userName})
+    },
+    call () {
+      this.$store.dispatch('call', { user: this.userName, peer: this.peerName })
+    },
+    endcall () {
+      this.$store.dispatch('endcall', {})
+    }
+  },
+  computed: {
+    counter () {
+      return this.$store.getters.getCounter
+    }
+  }
 }
 </script>
 
